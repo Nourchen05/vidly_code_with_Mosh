02be7cc85ff44a93dcf4c81523d3a45c6e9ee430
Nourchen05 <noamennourchen5@gmail.com>
2021-12-13 00:10:00 +0100

    first commit

---
+++ src/components/Movies.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends React.Component {
+  state = { movies: getMovies() };
+
+  handleDelete(movie) {
+    //console.log(movie);
+    //movies is a new array that not contains movie
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+  }
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
